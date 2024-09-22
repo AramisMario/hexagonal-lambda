6fbb31f146364c46bda007a9150da28c9a83202b
aramis <aramis.ortega@bam.com.gt>
2024-09-21 19:45:45 -0500

    Adding Address Value Object

---
+++ src/domain/valueObjects/Address.ts
@@ -0,0 +1,29 @@
+export interface AddressProperties{
+    country: string,
+    state: string,
+    city: string,
+    postalCode: string,
+    addres: string
+}
+
+export class Address{
+
+    private country: string;
+    private state: string;
+    private city: string;
+    private postalCode: string;
+    private addres: string;
+
+    constructor(address:AddressProperties){
+        this.country = address.country;
+        this.state = address.state;
+        this.city = address.city;
+        this.postalCode = address.postalCode;
+        this.addres = address.addres;
+    }
+
+    // logic for validate de addres;
+    validate(){
+
+    }
+}
