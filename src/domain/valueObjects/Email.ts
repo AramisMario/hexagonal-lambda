f5e9896b8a4bd2b84f3b60b294657d73b313fef4
aramis <aramis.ortega@bam.com.gt>
2024-09-21 19:54:46 -0500

    Adding Email Value-Object

---
+++ src/domain/valueObjects/Email.ts
@@ -0,0 +1,15 @@
+export class Email{
+    private email:string;
+
+    constructor(email:string){
+        this.email = email;
+    }
+
+    public validate(){
+
+    }
+
+    public getEmial(){
+        return this.email;
+    }
+}
